4c149db21900e19177060d3fb4dc8f141b3e192f
Gavin <gblack@d1stkfactory>
2014-08-02 00:10:47 +0000

    Recaptcha check now in code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,7 +36,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     respond_to do |format|
-      if @user.save
+  
+      if verify_recaptcha(:model => @user, :message => "Oh! It's error with reCAPTCHA!") && @user.save
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render action: 'show', status: :created, location: @user }
       else
