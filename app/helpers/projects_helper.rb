80a0bb857917e88a31c910e4161a9e73ebb60282
Gavin <gblack@d1stkfactory>
2014-08-08 11:34:55 +0000

    Updated completion ratio display

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,8 @@
 module ProjectsHelper
+
+def ratio_string(numer, total)
+   percent = number_with_precision(100*numer.to_f / total.to_f, :precision => 2)
+   "#{numer} / #{total} (#{percent}%)"
+end
+
 end
